814d13e1714ad2c3ed2fdbedfcc786ab617ab7b9
ujjwaltwitx <ujjwalcpj123@gmail.com>
2023-11-02 23:22:16 +0530

    add:features::save words, search from saved words, remove saved words, localStorage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const meaningsBox = document.getElementById("meaningsBox");
-async function fetchMeaning() {
-    meaningsBox.innerHTML = ""
-    const inputData = document.getElementsByClassName("input-box")[0].value;
-    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+inputData;
+async function fetchMeaning(text) {
+    meaningsBox.innerHTML = "";
+    const inputData = text ?? document.getElementsByClassName("input-box")[0].value;
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + inputData;
     const data = await fetch(url, {
-        method : "GET",
+        method: "GET",
     })
-    if(data.status != 200){
+    if (data.status != 200) {
         meaningsBox.innerHTML = "";
         console.log("error");
         meaningsBox.innerHTML += `<p>The word you searched for is out of this planet</p>`
@@ -14,8 +14,8 @@ async function fetchMeaning() {
     }
     const jsonData = await data.json();
     const meanings = jsonData[0].meanings;
-    
-    for(var i = 0; i<meanings.length; i++){
+
+    for (var i = 0; i < meanings.length; i++) {
         meaningsBox.innerHTML += `<div class="meaning-card">
         <div id="part-of-speech">
             ${meanings[i].partOfSpeech}
@@ -35,31 +35,48 @@ function saveWord() {
     getSavedWords();
 }
 
-function getSavedWords(){
+function getSavedWords() {
     const saved_words_box = document.getElementById('saved_words');
     saved_words_box.innerHTML = "";
-    for(var i = 0; i<localStorage.length; i++){
-        saved_words_box.innerHTML += `<div class="saved_word_chip">${localStorage.key(i)}<div id="cancel-icon"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    for (var i = 0; i < localStorage.length; i++) {
+        const chipElement = document.createElement("div");
+        const iconElement = document.createElement("div");
+        iconElement.innerHTML = `<div id="cancel-icon"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-      </svg></div>
-      </div>`
+        </svg></div>`
+        chipElement.id = localStorage.key(i);
+        chipElement.innerHTML = `<div class="saved_word_chip">${localStorage.key(i)}</div>`
+        chipElement.append(iconElement)
+        iconElement.addEventListener('click', ()=>{
+            document.getElementsByClassName("input-box")[0].value = chipElement.id;
+            localStorage.removeItem(chipElement.id);
+            getSavedWords();
+        })
+        chipElement.addEventListener('click', ()=>{
+            document.getElementsByClassName("input-box")[0].value = chipElement.id;
+            fetchMeaning(chipElement.id);
+        })
+        saved_words_box.appendChild(chipElement);
     }
 }
 
-const searchIcon = document.getElementsByClassName("search-icon")[0]
+
+
+const wordMeaningChip = document.getElementById('word')
+const cancelbutton = document.getElementById('cancel-icon');
+const searchIcon = document.getElementsByClassName("search-icon")[0];
 const saveWordButton = document.getElementsByClassName("save-word-button")[0];
 const inputBox = document.getElementsByClassName("input-box")[0];
 
-inputBox.addEventListener('input', ()=>{
+inputBox.addEventListener('input', () => {
     saveWordButton.setAttribute('style', "visibility:hidden")
 })
-
-searchIcon.addEventListener('click', function() {
+searchIcon.addEventListener('click', function () {
     fetchMeaning();
 })
 saveWordButton.addEventListener('click', saveWord);
 
-window.onload = ()=>{
+window.onload = () => {
     document.getElementsByClassName("save-word-button")[0].setAttribute('style', "visibility:hidden");
     getSavedWords();
 };
